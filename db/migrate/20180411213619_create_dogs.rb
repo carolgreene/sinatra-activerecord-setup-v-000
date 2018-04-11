5d08caca52d330c692532ff60a7cba51156241e5
Carol Greene <carolgreene3@gmail.com>
2018-04-11 21:43:45 +0000

    automatically backed up by learn

--- db/migrate/20180411213619_create_dogs.rb
+++ db/migrate/20180411213619_create_dogs.rb
@@ -1,13 +1,22 @@
 class CreateDogs < ActiveRecord::Migration
 
-  def up
+  def change 
     create_table :dogs do |t|
-      t.string :name
-      t.string :breed
-    end
+      t.string :name 
+      t.string :breed 
+    end 
   end
+end 
 
-  def down
-    drop_table :dogs
-  end
-end
+ #method listed above does the same as these 2 methods all by itself
+  #def up
+    #create_table :dogs do |t|
+      #t.string :name
+      #t.string :breed
+    #end
+  #end
+
+  #def down
+    #drop_table :dogs
+  #end
+#end
